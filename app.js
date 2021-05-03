9a4c2e028ec6302cfabc2f3db2c93c10ece6749c
jerso <jerso@LAPTOP-OF1OALBO>
2021-05-03 19:53:51 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,70 @@
+require('colors');
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const {
+    inquirerMenu,
+    pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoChecklist
+} = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
+
+const main = async() => {
+    let opt = '';
+    const tareas = new Tareas();
+    const tareasDB = leerDB();
+    if (tareasDB) {
+        tareas.cargarTareasFromArray(tareasDB);
+    }
+
+    do {
+        //Imprimir el menú
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case '1':
+                //Crear Opcion
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea(desc);
+                break;
+
+            case '2':
+                tareas.listadoCompleto();
+                break;
+            case '3':
+                //Completadas
+                tareas.listarPendientesCompletadas();
+                break;
+            case '4':
+                //Pendientes
+                tareas.listarPendientesCompletadas(false);
+                break;
+            case '5':
+                //Completado | pendiente
+                const ids = await mostrarListadoChecklist(tareas.listadoArr);
+                tareas.toggleCompletadas(ids);
+
+                break;
+            case '6':
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if (id !== '1') {
+                    const ok = await confirmar('¿Estas seguro de borrar esta tarea?');
+                    if (ok) {
+                        tareas.borrarTarea(id);
+                        console.log("Tarea borrada con exito");
+                    }
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        guardarDB(tareas.listadoArr);
+        await pausa();
+
+    } while (opt !== '7');
+};
+
+main();
